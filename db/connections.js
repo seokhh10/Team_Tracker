9d8903f9384669de54fd98d20ba334c195d35a5b
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-07-20 11:44:04 -0500

    Established connection to the database and make sure the options menu works

---
+++ db/connections.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+// Assign Port
+const PORT = process.env.PORT || 3001;
+
+// Connect to database
+const pool = new Pool(
+  {
+    user: 'postgres',
+    password: 'seok3525!@',
+    host: 'localhost',
+    database: 'teamtracker_db'
+  },
+  console.log(`Connected to the teamtracker_db database.`)
+);
+
+pool.connect()
+  .then(() => console.log('Connected to the PostgreSQL database'))
+  .catch(err => console.error('Error connecting to the database:', err));
+
--- server.js
+++ server.js
@@ -1,18 +1,32 @@
+// Dependencies
 const inquirer = require("inquirer");
+//const table = ("console.table");
+//SQL db
+const db = require("./db/connections");
+//Options prompts
 const prompt = require("./util/options");
 
-
-
+// Call firstPrompt function after the module is loaded
 firstPrompt();
 
+
+
 function firstPrompt() {
-    //Menu
-    inquirer.prompt(prompt.firstPrompt).then(function({task}){
+    inquirer.prompt(prompt.firstPrompt).then(function({ task }) {
         switch (task) {
-            case "View all Employees":
-            viewEmployee();
-            break;
+            case "View All Employees":
+                viewEmployee();
+                break;
+            // Add other cases as needed
+            default:
+                console.log('Invalid selection');
+                break;
         }
     });
 }
 
+
+function viewEmployee() {
+    // Your logic to view employees
+    console.log("Viewing all employees...");
+}
